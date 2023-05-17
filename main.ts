1720bdb55ea06a88895c5131af123b8f70637fb2
jcarpon016 <132546629+jcarpon016@users.noreply.github.com>
2023-05-17 20:08:09 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+let fijar_b_a = 0
 basic.forever(function () {
-	
+    let b = 0
+    fijar_b_a = 255 - input.lightLevel()
+    led.setBrightness(b)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
